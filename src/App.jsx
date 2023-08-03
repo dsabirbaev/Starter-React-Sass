731715341612d5b617287dbb8db7293cc93653a8
Davranbek <davranbek_1995@mail.ru>
2023-08-03 08:42:33 +0500

    added starter

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+
+
+const App = () => {
+  return (
+    <div>
+      <h1>React starter</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/components/Layout/index.jsx
@@ -0,0 +1,12 @@
+
+import "./style.scss";
+
+const index = () => {
+    return (
+        <main>
+            <h1>Section</h1>
+        </main>
+    );
+};
+
+export default index;
