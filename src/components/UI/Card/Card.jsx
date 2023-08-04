4fa7b3c275711486eeb6887d1916b1fd3466b879
Davranbek <davranbek_1995@mail.ru>
2023-08-04 13:29:02 +0500

    added assets and ui

---
+++ src/components/UI/Card/Card.jsx
@@ -0,0 +1,14 @@
+
+
+import "./style.scss";
+
+const Card = (props) => {
+    const {text, img, desc, isBuy} = props.state;
+    return (
+        <div className="card">
+            {/* Card */}
+        </div>
+    );
+};
+
+export default Card;
